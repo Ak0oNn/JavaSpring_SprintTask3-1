b16374210f93285467c5dc209b431c585937f4c4
Ak0oNn <133880573+Ak0oNn@users.noreply.github.com>
2023-07-24 13:48:17 +0500

    Add files via upload

---
+++ src/test/java/kz/bitlab/JavaSpring_SprintTask1/JavaSpringSprintTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.JavaSpring_SprintTask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSprintTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
